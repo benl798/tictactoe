ed8a06ccda3b41339c05446daddc29a4fd7af312
benl798 <ben.l7@hotmail.com>
2020-07-20 17:14:18 +1000

    tic-tac-toe-push1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 console.log('Works');
+
+// 1. The size of the game map is 3×3.
+// 2. We have 2 players (X & O).
+// 3. Each player makes a single move in his turn.
+// 4. A move means marking a field in the map with the player’s mark.
+// 5. A player wins when he has a full row / column / diagonal.
+
+$(document).ready(function(){
+
+  console.log('DOM ready!');
+
+  $(document).on('mousemove', function(ev){
+
+
+  });
+
+
+
+
+$(document).ready(function(){
+
+  // console.log('DOM ready!');
+
+  $('.box').on('click', function(){
+    console.log('click working');
+  })  
+}); // on click
+
+}); // document.ready
