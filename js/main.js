c06a70df6b7872cf4999c1787134426811968c50
benl798 <ben.l7@hotmail.com>
2020-07-21 09:28:35 +1000

    upload

--- js/main.js
+++ js/main.js
@@ -6,25 +6,35 @@ console.log('Works');
 // 4. A move means marking a field in the map with the player’s mark.
 // 5. A player wins when he has a full row / column / diagonal.
 
-$(document).ready(function(){
+  $(document).ready(function(){
 
-  console.log('DOM ready!');
+    // console.log('DOM ready!');
 
-  $(document).on('mousemove', function(ev){
+    $('.box').on('click', function(){
+      console.log('click working');
+    })// end of  click
+
+// ----------------------------- Add tokens to board -----------------------------
+
+let turnSelect = 1;
+$('.box').on('click', function() {
+  if($(this).hasClass('filled') === true) {
+    alert('Already selected!');
+    return;
+  }
+  if(turnSelect % 2 === 0) {
+   $(this).css({'background-image': "url('images/token2.png')"});
+   $(this).addClass('Player-Two filled')
+   turnSelect++;
+ } else {
+   $(this).css({'background-image': "url('images/token1.png')"});
+   $(this).addClass('Player-One filled')
+   turnSelect++;
+ }
+
+ });
 
 
-  });
+  }); // end of document.ready
 
-
-
-
-$(document).ready(function(){
-
-  // console.log('DOM ready!');
-
-  $('.box').on('click', function(){
-    console.log('click working');
-  })  
-}); // on click
-
-}); // document.ready
+  
