e962c171f4e2ceb797aea3ea955dae129558f447
benl798 <ben.l7@hotmail.com>
2020-07-24 11:56:29 +1000

    cleaning code

--- js/main.js
+++ js/main.js
@@ -10,20 +10,20 @@ $('.box').on('click', function() {
 
 //------------------------------Alert for already filled space---------------
 
-  if($(this).hasClass('filled')) { // checks whether
+  if($(this).hasClass('filled')) { // Checks whether the owner on click has already been selected and has the class 'filled'
 
-    alert('Uh oh! This space has already been filled!');
+    alert('Uh oh! This space has already been filled!'); // Returns the alert if already filled
 
     return;
   }
 
 //--------------------------Player Move----------------------------------------------
 
-  $(this).css({'background-image': "url('images/symbol2.png')"}) // attaches symbol2 to the square selected
+  $(this).css({'background-image': "url('images/symbol2.png')"}) // Assigns symbol2 to the background image of the selected available square
 
-  $(this).addClass('Player-One filled') // gives two classes to the square selected
+  $(this).addClass('Player-One filled') // Adds two classes to the selected square
 
-  turnSelect++; //
+  turnSelect++; // Incriment operator that adds one (+1) to turnSelect
 
   let result = winCheck('Player-One');
 
@@ -31,6 +31,8 @@ $('.box').on('click', function() {
 
     return;
   }
+
+//-------------------------VS Player-Two-----------------------------------------
 
   // if winCheck = true
 
@@ -48,7 +50,6 @@ $('.box').on('click', function() {
   //   turnSelect++; // add 1 - next turn is player two's turn
   //   winCheck('Player-One'); // passing in player one
   // }
-
 
 //---------------------------CPU Player-------------------------------
 
@@ -72,19 +73,16 @@ $('.box').on('click', function() {
 
 }); // end of click handler
 
-//------------------------------Win Conditions------------------------------------------------
+//------------------------------Win Conditions-------------------------------------
 
   const winCheck = function(selected){
 
-    let winResult = false; // by default not won yet
-
-    // determines whether any of the possible solutions are true
+    let winResult = false;
 
     if ($('#1').hasClass(selected) && $('#2').hasClass(selected) && $('#3').hasClass(selected)) {
-      // console.log('winner'); test
-      winResult = true;
+      winResult = true; // Returns true if
 
-      $('#1,#2,#3').addClass('winningRow'); // select multiple boxes to add the winningRow class
+      $('#1,#2,#3').addClass('winningRow'); // Highlights the winning play (CSS created)
 
 
     } else if ($('#4').hasClass(selected) && $('#5').hasClass(selected) && $('#6').hasClass(selected)) {
@@ -130,7 +128,7 @@ $('.box').on('click', function() {
     }
 
     if(winResult) {
-      $('.winMessage').text(`${selected} wins!`)
+      $('.winMessage').text(`${selected} wins!`) //
 
 
       if (selected === 'Player-One'){ // curly brackets to run the code
@@ -141,8 +139,6 @@ $('.box').on('click', function() {
       }
        $('.winCount').text(`Player One: ${playerOneScore} CPU: ${cpuScore} `)
       return true;
-
-
 
 //---------------------------Draw result -------------------------------------
 
@@ -162,23 +158,20 @@ $('.box').on('click', function() {
         $('.winMessage').text(`It's a draw!`)
       }
     }
-  }; // end of winCheck
-
+  }; // End of winCheck
 
 //--------------------------------Reset Button------------------------------------
 
-// set win result back to default and remove images
-  $('.btn').on('click', function() { // reset the game once a player wins
-    // console.log('reset'); test
+  $('.btn').on('click', function() {
 
-    $('.box').css({'background-image': 'none'}) // removes all images (x & o's)
+    $('.box').css({'background-image': 'none'}) // Clears all symbols from the grid
 
-    $('.box').removeClass('Player-One CPU filled winningRow') // resets selected class for players
+    $('.box').removeClass('Player-One CPU filled winningRow') // Removes all classes previously assigned on the grid
 
-    $('.winMessage').text(' ') // resets the win message to empty
+    $('.winMessage').text(' ') // Replaces the win message to empty
 
-    turnSelect = 1; // resets turn number to 1
+    turnSelect = 1; // Resets turnSelect to 1
 
-  }); // end of reset button
+  }); // End of reset button
 
-}); // end of document.ready
+}); // End of document.ready
